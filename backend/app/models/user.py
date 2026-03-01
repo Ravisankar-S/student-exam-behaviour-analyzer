f15e72453546b7367cdea32fbb4bffb3611d639a
Ravisankar-S <147087194+Ravisankar-S@users.noreply.github.com>
2026-03-01 12:32:03 +0530

    backend-skeleton-intial

---
+++ backend/app/models/user.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, String, Enum, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from app.db.base import Base
+import enum
+from datetime import datetime
+
+class RoleEnum(str, enum.Enum):
+    student = "student"
+    teacher = "teacher"
+    admin = "admin"
+
+class AuthProviderEnum(str, enum.Enum):
+    local = "local"
+    google = "google"
+    github = "github"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=True)
+    role = Column(Enum(RoleEnum), nullable=False)
+    auth_provider = Column(Enum(AuthProviderEnum), default=AuthProviderEnum.local)
+    provider_id = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
